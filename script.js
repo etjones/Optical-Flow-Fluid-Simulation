7be3a233305b68b616425a78182ff39634a8f5d0
Evan Jones <evan.jones@accenture.com>
2019-12-10 12:41:56 -0600

    Moved custom code to separate `acc_script_additions.js` file so the index.js code is closer to the original intent. Note there are still some additions left there, such as the ability to specify splat colors, etc/

--- script.js
+++ script.js
@@ -24,125 +24,6 @@ SOFTWARE.
 
 'use strict';
 
-// ETJ DEBUG
-var DEFAULT_COLOR = {r:0.9, g:0.1, b:0.1};
-
-// // console.log-to-div, from: https://stackoverflow.com/a/20256785
-(function () {
-    var old = console.log;
-    var logger = document.getElementById('log');
-    console.log = function (message) {
-        old(message);
-        let asStr = message;
-        if (typeof message == 'object' && JSON && JSON.stringify) {
-            asStr = JSON.stringify(message, null, 4);
-        }
-        logger.innerHTML += asStr.replace('\n', '<br/>') + '<br />';
-    }
-})();
-
-function remap(inputVal, fromMin, fromMax, toMin, toMax){
-    // Mirror's TouchDesigner's tdu.remap
-    let ratio = (inputVal-fromMin)/(fromMax - fromMin);
-    return toMin + ratio * (toMax - toMin);
-}
-
-function touchFlare(startX, startY, length,  nauticalDegrees, durationMillis=500, color=DEFAULT_COLOR, id=42) {
-
-    // let startX, startY, and length be integer pixel values
-    // At some frame rate, send touch events simulating a finger in a straight line
-    // over `duration` seconds.
-    // ETJ DEBUG
-    console.log(`canvas: (${canvas.width}, ${canvas.height})`);
-    // END DEBUG
-    let endX, endY; 
-    [endX, endY] = polarVectorAdd(startX, startY, length, nauticalDegrees);
-    let el = document.getElementsByTagName('canvas')[0];
-    
-    let attrs = {identifier: id, color}
-    let startEvent = makeTouchStart(el, startX, startY, attrs);
-
-    id = startEvent.targetTouches[0].identifier;
-
-    let fps = 60; 
-    // let steps = fps * durationMillis / 1000; 
-    let steps = length/50;
-    let dt = durationMillis/steps;
-    for (let t = dt; t < durationMillis; t += dt){
-        let x = remap(t, 0, durationMillis, startX, endX);
-        let y = remap(t, 0, durationMillis, startY, endY);
-        let eventCallback = () => { 
-            makeTouchMove(el, x, y, attrs);
-        }
-        setTimeout( eventCallback, t);
-    }
-    // TODO: Do we need this touchend event?
-    setTimeout(() => {makeTouchEnd(el, endX, endY, attrs)}, durationMillis);
-
-}
-
-function polarVectorAdd(xPixels, yPixels, lengthPixels, nauticalDegrees){
-    // TODO: I think there's some important checking to do here to make up 
-    // for non-square pixels
-    let degrees = -(90 - nauticalDegrees)
-    let radians = degrees * 0.017453292519943295; // = pi/180
-    let endX = xPixels + lengthPixels * Math.cos(radians);
-    let endY = yPixels + lengthPixels * Math.sin(radians);
-    return [endX, endY];
-}
-
-// manually create touch event. See: https://gist.github.com/morewry/538efb737ed9c4e432e4
-function makeTouchEvent(eventName="touchstart"){
-    // Different browsers may throw errors differently? First block fails on 
-    // my desktop Firefox, but second block succeeds 
-    // -ETJ 04 December 2019
-    let e;
-    try{
-        e = document.createEvent('TouchEvent')
-        e.initTouchEvent(eventName, true, true)
-    }
-    catch (err){
-        try{
-            e = document.createEvent('UIEvent')
-            e.initUIEvent(eventName, true, true)
-        }
-        catch( err){
-            e = document.createEvent('Event')
-            e.initEvent(eventName, true, true)
-        }
-    }
-    return e;
-}
-
-function makeTouchObject(x=0, y=0, extraAttrs=null){
-    let obj = {pageX: x, pageY: y};
-    if (extraAttrs){
-        obj = Object.assign(obj, extraAttrs);
-    }
-    return obj;
-}
-
-function makeTouchStart(el, x = 0, y = 0, extraAttrs=null){
-    let e = makeTouchEvent("touchstart");
-    e.targetTouches = [makeTouchObject(x,y,extraAttrs)];
-    el.dispatchEvent(e);
-    return e;
-} 
-
-function makeTouchMove(el, x=0, y=0, extraAttrs=null){
-    let e = makeTouchEvent("touchmove");
-    e.targetTouches = [makeTouchObject(x,y,extraAttrs)];
-    el.dispatchEvent(e);
-    return e;
-}
-
-function makeTouchEnd(el, x = 0, y = 0, extraAttrs=null){
-    let e = makeTouchEvent("touchend");
-    e.changedTouches = [makeTouchObject(x,y,extraAttrs)];
-    el.dispatchEvent(e);
-    return e;
-}
-// END DEBUG
 
 const canvas = document.getElementsByTagName('canvas')[0];
 resizeCanvas();
@@ -175,11 +56,6 @@ let config = {
     SUNRAYS_WEIGHT: 1.0,
 }
 
-// ETJ DEBUG
-config.COLORFUL = false; // If COLORFUL, our colors get changed on us
-// config.TRANSPARENT = true;
-// END DEBUG
-
 function pointerPrototype () {
     this.id = -1;
     this.texcoordX = 0;
@@ -193,6 +69,9 @@ function pointerPrototype () {
     this.color = [30, 0, 300];
 }
 
+// ===========
+// = GLOBALS =
+// ===========
 let pointers = [];
 let splatStack = [];
 pointers.push(new pointerPrototype());
@@ -1555,6 +1434,9 @@ function correctRadius (radius) {
     return radius;
 }
 
+// ====================
+// = USER INTERACTION =
+// ====================
 canvas.addEventListener('mousedown', e => {
     let posX = scaleByPixelRatio(e.offsetX);
     let posY = scaleByPixelRatio(e.offsetY);
